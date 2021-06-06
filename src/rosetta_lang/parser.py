bb437e2dfe371a4f59b8647d264d126e90fbeea8
Leandro Benedet Garcia <cerberus1746@gmail.com>
2021-06-05 22:41:58 -0300

    basic parsing is now complete

---
+++ src/rosetta_lang/parser.py
@@ -0,0 +1,44 @@
+import pathlib
+import lark
+from lark import exceptions
+
+from . import tree_indenter
+
+
+curr_path = pathlib.Path(__file__).parent
+
+
+class RosettaSyntaxError(SyntaxError):
+    pass
+
+
+lark_parser = lark.Lark.open(
+    str(curr_path / "rosetta.lark"),
+    parser="lalr",
+    maybe_placeholders=True,
+    regex=True,
+)
+
+
+def parse(file_path: pathlib.Path, to_parse):
+    try:
+        return lark_parser.parse(to_parse)
+    except(
+        exceptions.UnexpectedToken,
+        exceptions.UnexpectedCharacters
+    ) as parse_error:
+        error_tuple = (
+            str(file_path.resolve()),
+            str(parse_error.line),
+            str(parse_error.column),
+        )
+        error_str = "At: "
+        error_str += ":".join(error_tuple)
+
+        if isinstance(parse_error, exceptions.UnexpectedCharacters):
+            error_str += "\nexpected:\n  "
+            error_str += "\n  ".join(parse_error.allowed)
+
+        error_str += "\n" + str(parse_error)
+
+        raise RosettaSyntaxError(error_str)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,46 +1,58 @@
 import logging
 import os
-import pathlib
-import pprint
+from pathlib import Path
 
 import pytest
 
-current_folder = pathlib.Path(__file__).parent
+from rosetta_lang import parser
+
+
+current_folder = Path(__file__).parent
+
+work_dir = Path(os.getenv("TOX_WORK_DIR", current_folder))
+current_tox_env = os.getenv("TOX_ENV_NAME", "")
+
+log_file = work_dir / current_tox_env / "testing.log.md"
+
+file_logger = logging.FileHandler(log_file, "w")
 
 
 @pytest.fixture(scope="session")
 def curr_logger():
-    work_dir = pathlib.Path(os.getenv("TOX_WORK_DIR", current_folder))
-    current_tox_env = os.getenv("TOX_ENV_NAME", "")
-    file_logger = logging.FileHandler(work_dir / current_tox_env / "testing.log.rst", "w")
-
     curr_logger = logging.getLogger()
     curr_logger.setLevel(logging.DEBUG)
     curr_logger.addHandler(file_logger)
 
-    test_title = "PyTest"
-    test_title_separator = "=" * len(test_title)
-
-    curr_logger.debug(test_title_separator)
-    curr_logger.debug(test_title)
-    curr_logger.debug(test_title_separator + "\n")
+    curr_logger.debug("# PyTest\n")
 
     return curr_logger
 
 
 @pytest.fixture(scope="session")
-def text_to_parse():
-    with open(current_folder / "text_to_parse.roseta") as orig_text:
-        return orig_text.read()
+def open_text_to_parse():
+    def file_to_open(file_name: str):
+        path_to_open = current_folder / "resources/language_files" / file_name
+        with path_to_open.open() as orig_text:
+            return orig_text.read(), path_to_open
+
+    return file_to_open
 
 
 @pytest.fixture(scope="session")
 def debug_log_code(curr_logger):
-    def inner_debug_log_code(text_to_parse):
-        pp = pprint.PrettyPrinter(width=100)
-
-        out_str = pp.pformat(text_to_parse)
-        out_str = out_str.replace("\n", "\n    ")
-        curr_logger.debug("\n.. code :: python\n\n    " + out_str)
+    def inner_debug_log_code(curr_title, text_to_parse, parse_result):
+        curr_logger.debug(f"## {curr_title}")
+        curr_logger.debug("Code to be parsed:\n```\n" + text_to_parse + "```")
+        curr_logger.debug("Result:\n```\n" + parse_result.pretty() + "```")
 
     return inner_debug_log_code
+
+
+@pytest.fixture(scope="session")
+def parse_test(open_text_to_parse, debug_log_code):
+    def inner_parse_test(test_name, file_name):
+        to_parse, opened_file = open_text_to_parse(file_name)
+        result = parser.parse(opened_file, to_parse)
+        debug_log_code(test_name, to_parse, result)
+
+    return inner_parse_test
